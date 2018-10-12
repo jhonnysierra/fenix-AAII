34ed4075784da8a8c8942eddbb10d2969353ead5
jhonnysierra <jhonnysierrap@gmail.com>
2018-10-12 17:31:32 -0500

    Editar cliente GUI editar cliente
    Se crea la funcion que captura los datos del usuario y lo modifica

--- escritorio-fenix/src/main/java/proyectofenix/escritorio/controlador/EdicionClienteControlador.java
+++ escritorio-fenix/src/main/java/proyectofenix/escritorio/controlador/EdicionClienteControlador.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional.TxType;
+
+import com.sun.jdo.spi.persistence.support.ejb.ejbc.CMPROTemplateFormatter;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import proyectofenix.entidades.Cliente;
@@ -86,6 +92,27 @@ public class EdicionClienteControlador {
 	@FXML
 	private TextField cmpNoCuenta;
 
+	/**
+	 * Campo para la cuenta de telefono
+	 */
+	@FXML
+	private Label cmpInfoCrearCliente;
+
+	/**
+	 * Boton Aceptar de la ventana edicion cliente y crear cliente
+	 */
+	@FXML
+	private Button btnAceptar;
+
+	/**
+	 * Boton Aceptar de la ventana edicion cliente
+	 */
+	@FXML
+	private Button btnEditar;
+
+	/**
+	 * Genero de tipo enumeracion
+	 */
 	private Genero genero;
 
 	/**
@@ -98,7 +125,25 @@ public class EdicionClienteControlador {
 	 */
 	private ManejadorEscenarios manejador;
 
+	/**
+	 * Lista observable con las opciones del genero
+	 */
 	private ObservableList<String> itemsGenero;
+
+	/**
+	 * para almacenar clientes observables que se recibe desde detalle cliente
+	 */
+	private ObservableList<ClienteObservable> clientesObservablesDetalleCliente;
+
+	/**
+	 * Cliente que se envia como parametro a esta instancia
+	 */
+	private ClienteObservable clienteEditado;
+	
+	/**
+	 * Indice de la posicion en la lsita de clientes observables del cliente a editar
+	 */
+	private int indiceListaClientesObservables;
 
 	/**
 	 * Metodo constructor
@@ -114,7 +159,9 @@ public class EdicionClienteControlador {
 	 */
 	@FXML
 	private void initialize() {
+		cmpCedula.requestFocus();
 		cmpGenero.getItems().addAll(itemsGenero);
+		btnEditar.setVisible(false);
 	}
 
 	/**
@@ -123,6 +170,13 @@ public class EdicionClienteControlador {
 	 * @param cliente cliente a editar
 	 */
 	public void cargarPersona(ClienteObservable cliente) {
+		
+		btnAceptar.setVisible(false);
+		btnEditar.setVisible(true);
+		cmpInfoCrearCliente.setText("Por favor edite la información del cliente");
+		cmpCedula.setDisable(true);
+		cmpEmail.setEditable(false);
+		cmpNombre.requestFocus();
 
 		cmpCedula.setText(cliente.getCedula().getValue());
 		cmpNombre.setText(cliente.getNombre().getValue());
@@ -130,6 +184,20 @@ public class EdicionClienteControlador {
 		cmpEmail.setText(cliente.getEmail().getValue());
 		cmpClave.setText(cliente.getClave().getValue());
 		cmpFechaNacimiento.setValue(Utilidades.pasarALocalDate(cliente.getFechaNacimiento().getValue()));
+		cmpGenero.getSelectionModel().select(cliente.getGenero().getValue());
+		cmpTelefono.setText(cliente.getTelefono().getValue().get(0));
+		cmpDireccion.setText(cliente.getDireccion().getValue());
+		cmpNoCuenta.setText(cliente.getNumeroCuenta().getValue());
+
+		clientesObservablesDetalleCliente = manejador.getClientesObservables();
+
+		for (ClienteObservable c : manejador.getClientesObservables()) {
+			if (c.getCedula().getValue() == cliente.getCedula().getValue()) {
+				indiceListaClientesObservables = clientesObservablesDetalleCliente.indexOf(c);
+			}
+		}
+
+		// System.out.println("Indice: " + indiceListaClientesObservables);
 
 	}
 
@@ -139,7 +207,6 @@ public class EdicionClienteControlador {
 	@FXML
 	public void registrarCliente() {
 		int seleccionGenero;
-		seleccionGenero = cmpGenero.getSelectionModel().getSelectedIndex();
 
 		Cliente cliente = new Cliente();
 		cliente.setCedula(cmpCedula.getText());
@@ -154,6 +221,7 @@ public class EdicionClienteControlador {
 		cliente.setTelefonos(telefonos);
 		cliente.setDireccion(cmpDireccion.getText());
 		cliente.setEstado("1");
+		seleccionGenero = cmpGenero.getSelectionModel().getSelectedIndex();
 		if (seleccionGenero == 0) {
 			cliente.setGenero(genero.masculino);
 		} else {
@@ -173,9 +241,37 @@ public class EdicionClienteControlador {
 	 * permite editar la informacion de una persona
 	 */
 	@FXML
-	private void modificar() {
-		// TODO terminar modificar cliente
-		escenarioEditar.close();
+	private void editarCliente() {
+		int seleccionGenero;
+
+		Cliente cliente = new Cliente();
+		cliente.setCedula(cmpCedula.getText());
+		cliente.setNombres(cmpNombre.getText());
+		cliente.setApellidos(cmpApellido.getText());
+		cliente.setContrasenia(cmpClave.getText());
+		cliente.setCorreo(cmpEmail.getText());
+		cliente.setFecha_nacimiento(Utilidades.pasarADate(cmpFechaNacimiento.getValue()));
+		cliente.setCiudad(null);
+		cliente.setNoCuenta(cmpNoCuenta.getText());
+		telefonos.add(0, cmpTelefono.getText());
+		cliente.setTelefonos(telefonos);
+		cliente.setDireccion(cmpDireccion.getText());
+		cliente.setEstado("1");
+		seleccionGenero = cmpGenero.getSelectionModel().getSelectedIndex();
+		if (seleccionGenero == 0) {
+			cliente.setGenero(genero.masculino);
+		} else {
+			cliente.setGenero(genero.femenino);
+		}
+
+		if (manejador.editarCliente(cliente)) {
+			Utilidades.mostrarMensaje("Edición", "Se editó el cliente con éxito!");
+			clientesObservablesDetalleCliente.set(indiceListaClientesObservables, new ClienteObservable(cliente));
+			
+			escenarioEditar.close();
+		} else {
+			Utilidades.mostrarMensajeError("Edición", "Error en edición de cliente!");
+		}
 	}
 
 	/**
@@ -193,6 +289,7 @@ public class EdicionClienteControlador {
 	 */
 	public void setManejador(ManejadorEscenarios manejador) {
 		this.manejador = manejador;
+		this.cmpCedula.requestFocus();
 	}
 
 	/**
